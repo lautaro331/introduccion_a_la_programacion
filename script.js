9419979c8febc33d57ef2bafd1217cb89b881cd8
lautaro331 <caviglialautaro@gmail.com>
2025-10-01 23:48:50 +0000

    Variables Javascript

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const submit = document.getElementById('submit') 
-const input = document.getElementById('input') 
-console.log(submit.target)
-console.log(submit.target)
-input.onclick = (e) =>{
-    e.preventDefault()
-    console.log(input.value)
+const nombre ="Lautaro aaaaa"
+const saludo =  `Hola, soy ${nombre}`
+console.log(saludo)
+
+console.log("1"==1)
+console.log("1"===1)
+const lista=[1, "hola",null].length
+console.log(lista)
+function saludar(nombre){
+    console.log(`Hola ${nombre}` )
+    return"hola${nombre}"
 }
+
+saludar("mono")
+saludar(nombre)
+saludar(saludo)
+
+const misaludo=saludar("buenas")
+console.log(misaludo)
